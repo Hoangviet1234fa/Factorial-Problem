ed9bc3c147eee2c2215419c59bb1d314cb19dbdb
Nguyễn Viết Hoàng Việt <hoangviet1234fa@gmail.com>
2025-09-24 16:59:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Bài Toán Giai Thừa, tìm số từ 1 đến n
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Nhập n: ");
+        double n = Convert.ToDouble(Console.ReadLine());
+        double S = 1;
+        double i = 1;
+
+
+        while (i <= Convert.ToDouble(n))
+
+        {
+            S = S * i;
+            i = i + 1;
+            
+            
+        }
+        
+        Console.WriteLine($"Giai thừa của {n} là: {S}");
+    }
+
+}
+
+
+
+
+
+
